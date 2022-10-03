40ddf3b00368de7e9396d2550deb3b70655362e6
Driarake <adriana_drix@yahoo.com.br>
2022-10-02 21:44:38 -0300

    sacolaAPIIFood

---
+++ sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
@@ -0,0 +1,27 @@
+package me.dio.sacola.model;
+
+import java.util.List;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@NoArgsConstructor
+public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Produto> cardapio;
+    @Embedded
+    private Endereco endereco;
+}
